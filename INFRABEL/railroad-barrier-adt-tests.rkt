4cbe3151526e9aa7b071bd826a0419d1db36337a
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-10-19 21:11:37 +0200

    added test files aswell as added error messages to barrier-adt

---
+++ INFRABEL/railroad-barrier-adt-tests.rkt
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;         Railroad Barrier ADT Test         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#lang racket
+
+(require "../simulator/interface.rkt")
+(require rackunit)
+(require "railroad-barrier-adt.rkt")
+
+(setup-hardware)
+
