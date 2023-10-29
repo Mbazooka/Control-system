a4aa15a178cc74ea1eb2e7a7f26156ce21d794f4
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-10-29 19:39:04 +0100

    Added to tests, and changed switch module

--- railway-components/switch-adt.rkt
+++ railway-components/switch-adt.rkt
@@ -8,21 +8,26 @@
 (provide make-switch-adt)
 
 (define possible-switch-names '(S-1 S-2 S-3 S-4 S-5 S-6 S-7 S-8 S-9
-                                S-10 S-11 S-12 S-16 S-20 S-23 S-24
-                                S-25 S-26 S-27 S-28))
+                                    S-10 S-11 S-12 S-16 S-20 S-23 S-24
+                                    S-25 S-26 S-27 S-28))
 
-(define (make-switch-adt name)
+(define (make-switch-adt name initial-orientation)
+
+  ;; Test to make sure an incorrect switch cannot be made
   (unless (member name possible-switch-names)
-    (error "SWITCH-ADT: Invalid name"))
+    (error "SWITCH-ADT: Invalid name or orientation"))
 
-  (let ((state 'straight)) ;; 2 states, straight, rotate
+  (unless (or (eq? initial-orientation 'left) (eq? initial-orientation 'right))
+    (error "SWITCH-ADT: Invalid orientation given"))
+
+  (let ((state initial-orientation)) ;; 2 states, straight, rotate
 
     (define (current-position) state)
 
     (define (change-position! state)
       (cond
-        ((eq? state 'straight) (set! state 'straight))
-        ((eq? state 'deviate) (set! state 'deviate))
+        ((eq? state 'left) (set! state 'left))
+        ((eq? state 'right) (set! state 'right))
         (else
          "SWITCH-ADT: Incorrect state in change-position!")))
 
@@ -34,4 +39,3 @@
          "SWITCH-ADT: Incorrect message")))
     dispatch))
   
-                      
