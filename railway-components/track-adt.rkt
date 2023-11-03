28978d69f7113cbf4459d9bef67db9bcce50d29c
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-11-03 14:42:59 +0100

    finished track-adt and its tests for now

--- railway-components/track-adt.rkt
+++ railway-components/track-adt.rkt
@@ -6,6 +6,7 @@
 
 (provide make-track-adt)
 
+;; Track keeps link between different railway components (using names for efficiency, not actual objects)
 (define (make-track-adt name detection-block-name light-name switch-name barrier-name)
 
   (define (get-name) name)
@@ -21,10 +22,10 @@
   (define (dispatch msg)
     (cond
       ((eq? msg 'get-name) get-name)
-      ((eq? msg 'get-detection-block-name) detection-block-name)
-      ((eq? msg 'get-light-nae) light-name)
-      ((eq? msg 'switch-name) switch-name)
-      ((eq? msg 'barrier-name) barrier-name)
+      ((eq? msg 'get-detection-block-name) get-detection-block-name)
+      ((eq? msg 'get-light-name) get-light-name)
+      ((eq? msg 'get-switch-name) get-switch-name)
+      ((eq? msg 'get-barrier-name) get-barrier-name)
       (else
        "TRACK-ADT: Incorrect message")))
   dispatch)
