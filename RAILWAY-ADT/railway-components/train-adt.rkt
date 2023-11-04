5816430cb77bf5cdb7f2fb6c19c7f2d50f3f5786
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-11-04 22:19:58 +0100

    changed train-adt.rkt, tests to be changed

--- RAILWAY-ADT/railway-components/train-adt.rkt
+++ RAILWAY-ADT/railway-components/train-adt.rkt
@@ -7,41 +7,29 @@
 (#%require (only racket/base time error)) ;; Library for error messages
 (provide make-train-adt)
 
-(define (make-train-adt name initial-track initial-orientation)
-  (unless (or (eq? initial-orientation '+)
-              (eq? initial-orientation '-))
-    (error "TRAIN-ADT: Invalid orientation given"))
-  
-  (let ((speed 0)
-        (orientation initial-orientation)) ;; Orientation indicates direction of train (not negative speed)
+(define (make-train-adt name initial-track initial-track-behind)  
+  (let ((speed 0))
 
     (define (get-name) name)
 
     (define (get-initial-track) initial-track)
 
+    (define (get-initial-track-behind) initial-track-behind)
+
     (define (get-current-speed) speed)
 
-    (define (get-orientation) orientation)
-
-    (define (change-speed! input-speed) ;; Made stupidproof to avoid negative speeds
-      (if (or (> input-speed 300) (< input-speed 0)) ;; If not, speed can go too high or be negative
+    (define (change-speed! input-speed) ;; Made stupidproof to avoid high speeds
+      (if (> (abs input-speed) 300) ;; If not, speed can go too high
           "TRAIN-ADT: Illegal speed"
           (set! speed input-speed)))
-
-    (define (change-orientation! input-orientation)
-      (if (or (eq? input-orientation '+)
-              (eq? input-orientation '-))
-          (set! orientation input-orientation)
-          "TRAIN-ADT: Incorrect orientation"))
 
     (define (dispatch msg)
       (cond
         ((eq? msg 'get-name) get-name)
         ((eq? msg 'get-initial-track) get-initial-track)
+        ((eq? msg 'get-initial-track-behind) get-initial-track-behind)
         ((eq? msg 'get-current-speed) get-current-speed)
         ((eq? msg 'change-speed!) change-speed!)
-        ((eq? msg 'get-orientation) get-orientation)
-        ((eq? msg 'change-orientation!) change-orientation!)
         (else
          "TRAIN-ADT: Incorrect message")))
     dispatch))
