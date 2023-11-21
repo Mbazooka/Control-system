1917ced8b6743340f14e9fd60affdfc4a0eec89b
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-11-21 15:36:02 +0100

    Preliminary train tab gui logic

--- NMBS/GUI/GUI-adt.rkt
+++ NMBS/GUI/GUI-adt.rkt
@@ -17,7 +17,7 @@
 
     (define (dispatch msg)
       (cond
-        ((eq? msg 'get-counter) (get-counter))
+        ((eq? msg 'get-value) (get-counter))
         ((eq? msg 'increment!) (increment!))
         ((eq? msg 'decerement!) (decrement!))
         (else
@@ -25,7 +25,9 @@
     dispatch))
 
 (define trains-on-railway '()) ;; The trains currently on the railway
-(define train-counter (make-counter)) ;; Counter to keep up amount of trains         
+(define train-counter (make-counter)) ;; Counter to keep up amount of trains
+(define max-train-speed 200)
+(define min-train-speed -200)
 
 ;; Constants that determine frame size and title
 (define FRAME-WIDTH 800)
@@ -66,10 +68,10 @@
        ))
 
 ;; Draws a tab-panel for the different trains (in the train-tab)
-(define train-tabs '())
+(define train-tab '())
 
 (define (draw-train-tabs)
-  (set! train-tabs
+  (set! train-tab
         (new tab-panel%
              [parent top-panel]
              [choices trains-on-railway])))
@@ -80,7 +82,14 @@
        [parent main-tab-panel]
        ))
 
-;; Button to be added to the train-tab
+;; Button to be added to the train-tab and logic
+(define (tab-name-generator)
+  (train-counter 'increment!)
+  (format "Train-~a" (train-counter 'get-value)))
+
+(define (add-train-button-logic! panel event)
+  (send train-tab append (tab-name-generator)))
+
 (define add-train-button '())
 
 (define (make-add-train-button)
@@ -88,33 +97,62 @@
         (new button%
              [label "Add train"]
              [parent second-panel]
+             [callback add-train-button-logic!]
              [horiz-margin HORIZONTAL-OFFSET-ADD-TRAIN-BUTTON]
              )))
 
+
 ;; Button to be added to the train-tab
+(define (delete-trian-button-logic! panel event)
+  (send train-tab delete (send train-tab get-selection)))
+
 (define delete-train-button '())
 
 (define (make-delete-train-button)
   (set! delete-train-button
         (new button%
              [label "Delete train"]
-             [parent second-panel])))
-
-;; Message for train-tab
+             [parent second-panel]
+             [callback delete-trian-button-logic!])))
 
 ;; Initial drawing for tabs
 (draw-train-tabs)
 (make-add-train-button)
 (make-delete-train-button)
-(sleep 5)
+;(sleep 5)
+
+;; Draws a panel on top of the tab, in a vertical manner
+(define top-tab-panel
+    (new vertical-panel%
+         [parent train-tab]
+         ))
+
+;; Message on train-tab
+(define display-message (new message%
+                             [label "Set train speed"]
+                             [parent top-tab-panel]))
+
+;; Slider for train-tab
+(define slider '())
+
+(define (make-slider)
+  (set! slider (new slider%
+                    [label ""]
+                    [parent top-tab-panel]
+                    [min-value min-train-speed]
+                    [max-value max-train-speed]
+                    [init-value 0]
+                    [vert-margin 100])))
+
+(make-slider)
 
 ;; Remove the elements
 (define (remove-train-tab-elements)
-  (send train-tabs show #f)
+  (send train-tab show #f)
   (send add-train-button show #f)
   (send delete-train-button show #f))
 
-(remove-train-tab-elements)
+;(remove-train-tab-elements)
 
 
 
